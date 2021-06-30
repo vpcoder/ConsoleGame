19ef8533934f932a5144c2a84591f26f48f80688
LepJak <evgeny.kuprienko2003@yandex.ru>
2021-06-30 21:41:18 +0300

    Реализациия оружия ближнего боя

---
+++ ConsoleGame/Data/Objects/Items/Impls/ThieveDagger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Engine.Data
+{
+    public class ThieveDagger : MeleeWeapon
+    {
+        public ThieveDagger()
+        {
+            this.IsQuestItem = false;
+            this.MaxStackSize = 1;
+            this.StackSize = 1;
+            this.Damage = 15;
+            this.Symbol = '†';
+            this.Color = ConsoleColor.Blue;
+
+            this.Title = "Воровской кинжал";
+            this.Description = "\"Месть Марии\" - Кинжал воров из корабельных доков";
+        }
+    }
+}
